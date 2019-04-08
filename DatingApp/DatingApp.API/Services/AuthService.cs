0c9c6b9527a467e1787f06bdb39ef65cc73eb57b
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-04-09 07:38:39 +0800

    Implemented Login & UserExists methods

--- DatingApp/DatingApp.API/Services/AuthService.cs
+++ DatingApp/DatingApp.API/Services/AuthService.cs
@@ -1,13 +1,13 @@
-using System;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using DatingApp.API.Data;
 using DatingApp.API.Models;
 
 namespace DatingApp.API.Services
 {
-  public class AuthService : IAuthService
+    public class AuthService : IAuthService
   {
     private readonly DataContext _context;
 
@@ -15,9 +15,13 @@ namespace DatingApp.API.Services
     {
       this._context = context;
     }
-    public Task<User> Login(string username, string password)
+    public async Task<User> Login(string username, string password)
     {
-      throw new System.NotImplementedException();
+      User matchedUser = await this._context.Users.FirstOrDefaultAsync(_user => _user.Username == username);
+      if (matchedUser == null) {
+        return null;
+      }
+      return verifyPasswordHash(password, matchedUser.PasswordHash, matchedUser.PasswordSalt) ? matchedUser : null; 
     }
 
     public async Task<User> Register(User user, string password)
@@ -33,17 +37,30 @@ namespace DatingApp.API.Services
       return user;
     }
 
-    public Task<bool> UserExists(string username)
+    public async Task<bool> UserExists(string username)
     {
-      throw new System.NotImplementedException();
+      return (await this._context.Users.AnyAsync(_user => _user.Username == username));
     }
 
     private void createPasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
       using (HMACSHA512 hmacSha512 = new HMACSHA512()) {
-          passwordSalt = hmacSha512.Key;
-          passwordHash = Encoding.UTF8.GetBytes(password);
+        passwordSalt = hmacSha512.Key;
+        passwordHash = hmacSha512.ComputeHash(Encoding.UTF8.GetBytes(password));
       };
     }
+
+    private bool verifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt) {
+      using (HMACSHA512 hmacSha512 = new HMACSHA512(passwordSalt)) { 
+        var computedHash = hmacSha512.ComputeHash(Encoding.UTF8.GetBytes(password));
+        var hashLength = computedHash.Length;
+        for(int i=0; i<hashLength; i++) {
+          if (computedHash[i] != passwordHash[i]) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }    
   }
 }
