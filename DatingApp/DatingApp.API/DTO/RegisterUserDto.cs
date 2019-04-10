e3e969700a1def441c006a33b0e6710273cbb0d9
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-04-11 03:50:03 +0800

    Added required & stringLength validation attributes on RegisterUserDto

--- DatingApp/DatingApp.API/DTO/RegisterUserDto.cs
+++ DatingApp/DatingApp.API/DTO/RegisterUserDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTO
 {
     public class RegisterUserDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(64, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 64 characters.")]
         public string Password { get; set; }
     }
 }
